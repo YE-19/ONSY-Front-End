7b64ac1cfaf115f6f3c06db1649a1e0e69964cd2
Youssef ehab <ehab56526@gmail.com>
2026-04-10 01:38:31 +0200

    fix naveg

--- src/components/Home/Hero1.jsx
+++ src/components/Home/Hero1.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom'
 
 const Hero1 = () => {
+
+  const navigate = useNavigate();
+
   return (
     <section className=" pt-20 position-absolute top-0 bg-hero bg-cover bg-center h-screen w-full">
         <div className='m-30 flex gap-4 flex-col'>
          <h1 className=' text-[80px] font-medium w-147 h-[288] leading-24 font-Gabarito text-6xl bg-linear-to-br from-[#020103] via-onsy-primary to-onsy-secondary bg-clip-text text-transparent'>Ai That Helps Your Mental Health </h1>
           <p className='font-medium mt-8 text-[24px]'>Open up, express your feelings, and get AI <br></br>support to understand emotions, reduce stress, <br></br>and build a healthier mindset.</p>
-          <button  className='cursor-pointer w-45 h-12 bg-[#036464E5] text-[#111111] rounded-xl font-semibold'>Get Started</button>
+          <button onClick={() => navigate("/SingIn")} className='cursor-pointer w-45 h-12 bg-[#036464E5] text-[#111111] rounded-xl font-semibold'>Get Started</button>
       </div>
       </section>
   )
--- src/layout/MainNav.jsx
+++ src/layout/MainNav.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import { NavLink, useLocation } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const MainNav = () => {
 
+  const navigate = useNavigate();
   const location = useLocation();
   const isHomePage = location.pathname === '/';
 
   return (
       <>
       <section className=' w-full absolute top-0 px-24 gap-4 flex justify-between items-center flex-row content-between backdrop-blur z-10'>
-          <div className='text-[#147E8F]  font-labrada text-[48px] font-semibold mx-10'>
+          <div 
+          className='text-[#147E8F]  font-labrada text-[48px] font-semibold mx-10 cursor-pointer'
+          onClick={() => navigate("/")}
+          >
             ONSY
           </div>
           <nav className='links font-medium flex gap-8' >
@@ -30,10 +35,11 @@ const MainNav = () => {
             </NavLink>
           </nav>
           <button 
-          className={`h-12 border border-black rounded-xl text-white cursor-pointer transition-all ${
+          onClick={() => navigate("/SingIn")}
+          className={`h-12 border border-black rounded-xl text-white cursor-pointer transition-all w-45.25${
           isHomePage 
-          ? 'w-45.25 bg-transparent' 
-          : 'w-45.25 bg-[#036464E5]'
+          ? ' bg-transparent' 
+          : ' bg-[#036464E5]'
           }`}
           >
             Log in
